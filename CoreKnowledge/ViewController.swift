ae265ea371293fda69a8972c8e2f23554e376122
haoyongxin <hyx974@126.com>
2018-03-08 15:51:37 +0800

    first commit

---
+++ CoreKnowledge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreKnowledge
+//
+//  Created by haoyongxin on 2018/3/8.
+//  Copyright © 2018年 xinrui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
