125b7b41da39529c74a5eab914bf009b1ae4a4a2
haoyongxin <hyx974@126.com>
2018-03-08 16:04:16 +0800

    新增tabbar和navigation

---
+++ CoreKnowledge/Tabbar/CKTabbarViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CKTabbarViewController.swift
+//  CoreKnowledge
+//
+//  Created by haoyongxin on 2018/3/8.
+//  Copyright © 2018年 xinrui. All rights reserved.
+//
+
+import UIKit
+
+class CKTabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
