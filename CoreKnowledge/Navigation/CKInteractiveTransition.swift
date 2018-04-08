026b8ac83db8f1a1912d56f004461c4a54b96dde
haoyongxin <hyx974@126.com>
2018-04-08 17:27:49 +0800

    手势动画

---
+++ CoreKnowledge/Navigation/CKInteractiveTransition.swift
@@ -0,0 +1,13 @@
+//
+//  CKInteractiveTransition.swift
+//  CoreKnowledge
+//
+//  Created by haoyongxin on 2018/4/8.
+//  Copyright © 2018年 xinrui. All rights reserved.
+//
+
+import UIKit
+
+class CKInteractiveTransition: UIPercentDrivenInteractiveTransition {
+  
+}
